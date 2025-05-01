3d4060969eaf39ed914ef5dd53f53e56332fd040
Yash Rajoria <yashrajoria@gmail.com>
2025-05-01 11:19:23 +0530

    Refactor Product model: Add Description field and adjust formatting

--- backend/services/product-service/models/products.go
+++ backend/services/product-service/models/products.go
@@ -1,12 +1,19 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Product struct {
-	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name     string             `json:"title" bson:"title"`
-	Price    float64            `json:"price" bson:"price"`
-	Category primitive.ObjectID `json:"category" bson:"category"` // Store only category ID
-	Images   []string           `json:"images" bson:"images"`
-	Quantity int                `json:"quantity" bson:"quantity"`
+	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name        string             `json:"title" bson:"title"`
+	Price       float64            `json:"price" bson:"price"`
+	Category    primitive.ObjectID `json:"category" bson:"category"` // Store only category ID
+	Images      []string           `json:"images" bson:"images"`
+	Quantity    int                `json:"quantity" bson:"quantity"`
+	Description string             `json:"description" bson:"description"`
+	CreatedAt   time.Time          `gorm:"autoCreateTime"`
+	UpdatedAt   time.Time          `gorm:"autoUpdateTime"`
 }
