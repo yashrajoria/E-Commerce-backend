8552956d3e14837639217ca75b7819bc6f690c34
Yash Rajoria <yashrajoria@gmail.com>
2025-05-04 03:38:00 +0530

    refactor: add bulk product creation route to product service

--- backend/services/product-service/routes/routes.go
+++ backend/services/product-service/routes/routes.go
@@ -11,6 +11,7 @@ func RegisterProductRoutes(r *gin.Engine) {
 		productRoutes.GET("/", controllers.GetProducts)
 		productRoutes.GET("/:id", controllers.GetProductByID)
 		productRoutes.POST("/", controllers.CreateProduct)
+		productRoutes.POST("/bulk", controllers.CreateBulkProducts)
 		productRoutes.PUT("/:id", controllers.UpdateProduct)
 		productRoutes.DELETE("/:id", controllers.DeleteProduct)
 	}
