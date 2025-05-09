b3e97cc506e5ac314ac31407a45b54f7644e164d
Yash Rajoria <yashrajoria@gmail.com>
2025-05-09 11:40:37 +0530

    refactor: update Category and Product models to include timestamps and improve structure

--- backend/services/product-service/models/category.go
+++ backend/services/product-service/models/category.go
@@ -1,10 +1,19 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Category struct {
 	ID        primitive.ObjectID   `json:"_id,omitempty" bson:"_id,omitempty"`
 	Name      string               `json:"name" bson:"name"`
 	ParentIDs []primitive.ObjectID `json:"parent_ids,omitempty" bson:"parent_ids,omitempty"`
 	Ancestors []primitive.ObjectID `json:"ancestors,omitempty" bson:"ancestors,omitempty"`
+	Slug      string               `json:"slug" bson:"slug"`                       // For SEO-friendly URLs
+	Path      []string             `json:"path,omitempty" bson:"path,omitempty"`   // ["Mobile", "Android", "Samsung"]
+	Level     int                  `json:"level,omitempty" bson:"level,omitempty"` // Depth in hierarchy, optional for sorting
+	CreatedAt time.Time            `json:"created_at" bson:"created_at"`
+	UpdatedAt time.Time            `json:"updated_at" bson:"updated_at"`
 }
--- backend/services/product-service/models/products.go
+++ backend/services/product-service/models/products.go
@@ -7,13 +7,15 @@ import (
 )
 
 type Product struct {
-	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name        string             `json:"title" bson:"title"`
-	Price       float64            `json:"price" bson:"price"`
-	Category    primitive.ObjectID `json:"category" bson:"category"` // Store only category ID
-	Images      []string           `json:"images" bson:"images"`
-	Quantity    int                `json:"quantity" bson:"quantity"`
-	Description string             `json:"description" bson:"description"`
-	CreatedAt   time.Time          `json:"createdAt" bson:"createdAt"`
-	UpdatedAt   time.Time          `json:"updatedAt" bson:"updatedAt"`
+	ID           primitive.ObjectID   `bson:"_id,omitempty" json:"_id"`
+	Name         string               `json:"name"`
+	Price        float64              `json:"price"`
+	Quantity     int                  `json:"quantity"`
+	Description  string               `json:"description"`
+	Images       []string             `json:"images"`
+	CategoryID   primitive.ObjectID   `bson:"category_id" json:"category_id"`
+	CategoryIDs  []primitive.ObjectID `bson:"category_ids,omitempty" json:"category_ids"`
+	CategoryPath []string             `bson:"category_path,omitempty" json:"category_path"`
+	CreatedAt    time.Time            `json:"createdAt" bson:"createdAt"`
+	UpdatedAt    time.Time            `json:"updatedAt" bson:"updatedAt"`
 }
