aa28c96bdf074d9be2baf9bae42ea6baee36a617
Yash Rajoria <yashrajoria@gmail.com>
2025-05-09 10:26:39 +0530

    refactor: update Category model to support multiple parent IDs and ancestors

--- backend/services/product-service/models/category.go
+++ backend/services/product-service/models/category.go
@@ -3,7 +3,8 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Category struct {
-	ID       primitive.ObjectID  `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name     string              `json:"name" bson:"name"`
-	ParentID *primitive.ObjectID `json:"parent_id,omitempty" bson:"parent_id,omitempty"` // Nullable for top-level categories
+	ID        primitive.ObjectID   `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name      string               `json:"name" bson:"name"`
+	ParentIDs []primitive.ObjectID `json:"parent_ids,omitempty" bson:"parent_ids,omitempty"`
+	Ancestors []primitive.ObjectID `json:"ancestors,omitempty" bson:"ancestors,omitempty"`
 }
