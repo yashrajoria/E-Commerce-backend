41b6736421738794200d53fbc6e36471e73770a3
Yash <yashrajoria@gmail.com>
2025-08-07 22:37:54 +0530

    refactor: clean up Product struct formatting in products.go

--- backend/services/product-service/models/products.go
+++ backend/services/product-service/models/products.go
@@ -7,15 +7,15 @@ import (
 )
 
 type Product struct {
-	ID           uuid.UUID   `bson:"_id" json:"_id"`
-	Name         string      `json:"name"`
-	Price        float64     `json:"price"`
-	Quantity     int         `json:"quantity"`
-	Description  string      `json:"description"`
-	Images       []string    `json:"images"`
-	Brand        string      `json:"brand"`
-	SKU          string      `json:"sku"`
-	CategoryID   uuid.UUID   `bson:"category_id" json:"category_id"`
+	ID          uuid.UUID `bson:"_id" json:"_id"`
+	Name        string    `json:"name"`
+	Price       float64   `json:"price"`
+	Quantity    int       `json:"quantity"`
+	Description string    `json:"description"`
+	Images      []string  `json:"images"`
+	Brand       string    `json:"brand"`
+	SKU         string    `json:"sku"`
+	// CategoryID   uuid.UUID   `bson:"category_id" json:"category_id"`
 	CategoryIDs  []uuid.UUID `bson:"category_ids,omitempty" json:"category_ids"`
 	CategoryPath []string    `bson:"category_path,omitempty" json:"category_path"`
 	CreatedAt    time.Time   `json:"createdAt" bson:"createdAt"`
